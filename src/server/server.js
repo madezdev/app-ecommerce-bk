c6252ded7fa771b65d30387ee68ed3a8d2d377c9
Martin Hernandez <madezdev@gmail.com>
2025-02-07 06:49:56 -0300

    Agregar configuración del servidor y scripts de desarrollo. express configurado

---
+++ src/server/server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import cors from 'cors'
+
+export class Server {
+
+  constructor() {
+    this.app = express()
+    this.port = process.env.PORT || 8080
+    this.app.use(cors())
+    this.app.use(express.json())
+    this.middlewares()
+    this.routes()
+
+  }
+
+  middlewares() {
+    this.app.use(express.json())
+    this.app.use(cors())
+  }
+
+  routes() {
+    this.app.get('/', (req, res) => {
+      res.send('Hello World')
+    })
+  }
+
+  start() {
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`)
+    })
+  }
+}
