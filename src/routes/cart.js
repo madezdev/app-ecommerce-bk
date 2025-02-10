7b20d4f2492e77d9784e9f59d92ffcf48cad60b5
Martin Hernandez <madezdev@gmail.com>
2025-02-09 21:17:11 -0300

    Agregar funcionalidades para gestionar carritos: obtener todos los carritos, actualizar la cantidad de un producto y eliminar un producto del carrito.

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -3,6 +3,9 @@ import { CartControllers } from '../controllers/cart.js'
 
 export const cartRouter = Router()
 
+cartRouter.get('/', CartControllers.getAllCarts)
 cartRouter.get('/:id', CartControllers.getCartById)
 cartRouter.post('/', CartControllers.createCart)
 cartRouter.post('/:id/product/:productId', CartControllers.addProductToCart)
+cartRouter.put('/:id/product/:productId/quantity/:quantity', CartControllers.updateProductInCart)
+cartRouter.delete('/:id/product/:productId', CartControllers.deleteProductFromCart)
